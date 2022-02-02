4d771d17019719f898a49dae5b86b910d8510a18
Hilquias Camelo <hilquiaswpc10@outlook.com>
2022-02-02 14:30:36 -0300

    Criando service e repository para Gategoria

---
+++ src/main/java/com/hilquias/repositories/CategoriaRepository.java
@@ -0,0 +1,17 @@
+package com.hilquias.repositories;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.hilquias.domain.Categoria;
+
+
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer>  {
+
+	Optional<Categoria> findById(Integer id);
+	
+}
