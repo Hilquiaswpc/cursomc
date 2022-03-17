cddc232ed5491ec244c715672adac498352982fa
C1325732 HILQUIAS WILLAMS PEREIRA CAMELO <c1325732@interno.bb.com.br>
2022-03-17 19:33:03 -0300

    Inserindo novo cliente com POST

--- src/main/java/com/hilquias/resources/ClienteResource.java
+++ src/main/java/com/hilquias/resources/ClienteResource.java
@@ -2,14 +2,20 @@ package com.hilquias.resources;
 
 
 
+import com.hilquias.domain.Categoria;
 import com.hilquias.domain.Cliente;
+import com.hilquias.dto.CategoriaDTO;
 import com.hilquias.dto.ClienteDTO;
+import com.hilquias.dto.ClienteNewDTO;
 import com.hilquias.services.ClienteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.validation.Valid;
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -60,5 +66,14 @@ public class ClienteResource {
 		Page<ClienteDTO> listDto = list.map(obj -> new ClienteDTO(obj));
 		return ResponseEntity.ok().body(listDto);
 
+	}
+
+	@RequestMapping(method=RequestMethod.POST)
+	public ResponseEntity<Void> insert(@Valid @RequestBody ClienteNewDTO objDto) {
+		Cliente obj = service.fromDTO(objDto);
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 }
