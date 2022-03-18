df852514395b750d822eb9716d67adc78bcd97b8
C1325732 HILQUIAS WILLAMS PEREIRA CAMELO <c1325732@interno.bb.com.br>
2022-03-17 21:15:40 -0300

    Ajustando delecao em cascata

--- src/main/java/com/hilquias/domain/Cliente.java
+++ src/main/java/com/hilquias/domain/Cliente.java
@@ -22,7 +22,7 @@ public class Cliente  implements Serializable {
 
 
  
-    @OneToMany(mappedBy="cliente")
+    @OneToMany(mappedBy="cliente" , cascade = CascadeType.ALL)
     private List<Endereco> enderecos = new ArrayList<>();
 
     @ElementCollection
